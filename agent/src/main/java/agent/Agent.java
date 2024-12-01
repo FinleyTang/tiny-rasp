192b6c95861f210318a45a599ebd8ffc9407254a
FinleyTang <71133371+FinleyTang@users.noreply.github.com>
2024-12-01 23:30:58 +0800

    1、update javassist to 3.24.1-GA to fix readobject problems 2、complete a simple Deserialization  demo

--- agent/src/main/java/agent/Agent.java
+++ agent/src/main/java/agent/Agent.java
@@ -37,27 +37,72 @@ public class Agent {
                     }
                 }
 
-                if ("java/lang/ProcessBuilder".equals(s)) {
-                    System.out.println("Captured ProcessBuilder class");
+//                if ("java/lang/ProcessBuilder".equals(s)) {
+//                    System.out.println("Captured ProcessBuilder class");
+//
+//                    try {
+//                        ClassPool pool = ClassPool.getDefault();
+//                        CtClass processBuilderClass = pool.get("java.lang.ProcessBuilder");
+//
+//                        // 获取 start() 方法
+//                        CtMethod startMethod = processBuilderClass.getDeclaredMethod("start");
+//
+//                        // 插入代码来打印命令
+//                        startMethod.insertBefore("{ " +
+//                                "System.out.println(\"ProcessBuilder command: \" + this.command().toString());" +  // 使用 toString() 打印命令列表
+//                                "new Exception(\"Call stack:\").printStackTrace();" +
+//                                "}");
+//
+//                        byte[] byteCode = processBuilderClass.toBytecode();
+//                        processBuilderClass.detach();
+//                        return byteCode;
+//                    } catch (Exception ex) {
+//                        ex.printStackTrace();
+//                    }
+//                }
+
+                if ("java/io/ObjectInputStream".equals(s)) {
+                    System.out.println("Class: " + s);
 
                     try {
                         ClassPool pool = ClassPool.getDefault();
-                        CtClass processBuilderClass = pool.get("java.lang.ProcessBuilder");
+                        System.out.println("flag01");
 
-                        // 获取 start() 方法
-                        CtMethod startMethod = processBuilderClass.getDeclaredMethod("start");
+                        CtClass ctClass = pool.get("java.io.ObjectInputStream");
+                        System.out.println("flag02");
 
-                        // 插入代码来打印命令
-                        startMethod.insertBefore("{ " +
-                                "System.out.println(\"ProcessBuilder command: \" + this.command().toString());" +  // 使用 toString() 打印命令列表
-                                "new Exception(\"Call stack:\").printStackTrace();" +
-                                "}");
+                        // More specific method resolution
+                        // 获取 readObject 方法
+                        CtMethod[] methods = ctClass.getDeclaredMethods();
+                        for (CtMethod method : methods) {
+                            System.out.println(method.getName());
+                        }
+                        CtMethod readObjectMethod = ctClass.getDeclaredMethod("readObject");
+//                        CtMethod readObjectMethod = ctClass.getDeclaredMethod("readObject", new CtClass[] { pool.get("java.io.ObjectInputStream") });
 
-                        byte[] byteCode = processBuilderClass.toBytecode();
-                        processBuilderClass.detach();
+                        System.out.println("flag03");
+
+                        // 插桩：打印反序列化堆栈信息与输入数据类型
+                        // 插桩：打印反序列化堆栈信息与输入数据类型
+                        // 插桩代码：反序列化前打印信息
+                        String insertCode =
+                                "System.out.println(\"Deserialization started...\");" +
+                                        "System.out.println(\"Current Thread: \" + Thread.currentThread().getName());" +
+                                        "System.out.println(\"Calling method: \" + $0.getClass().getName() + \".readObject\");" +  // 打印调用的类与方法
+                                        "System.out.println(\"Stack Trace:\");" +
+                                        "new Exception(\"Call Stack:\").printStackTrace();" +  // 打印堆栈信息
+                                        "System.out.println(\"Deserializing Object\");";  // 简化目标对象类型打印
+
+                        readObjectMethod.insertBefore(insertCode);
+                        byte[] byteCode = ctClass.toBytecode();
+                        ctClass.detach();
                         return byteCode;
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
+                    } catch (NotFoundException e) {
+                        System.err.println("Class or method not found: " + e.getMessage());
+                        e.printStackTrace();
+                    } catch (Exception e) {
+                        System.err.println("Unexpected error: " + e.getMessage());
+                        e.printStackTrace();
                     }
                 }
 
--- other/src/main/java/other/Main.java
+++ other/src/main/java/other/Main.java
@@ -1,6 +1,6 @@
 package other;
 
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
 
@@ -12,6 +12,26 @@ public class Main {
         System.out.printf("result is: %d", result);
         Runtime runtime = Runtime.getRuntime();
         runtime.exec("cmd.exe /c dir");  // 在这里执行你要监控的命令
+
+        try {
+            // 创建一个对象并将其序列化
+            MyObject myObject = new MyObject("test", 123);
+            FileOutputStream fos = new FileOutputStream("myobject.ser");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(myObject);
+            oos.close();
+            System.out.println("Object serialized");
+
+            // 从文件中读取并反序列化
+            FileInputStream fis = new FileInputStream("myobject.ser");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            MyObject deserializedObject = (MyObject) ois.readObject();
+            ois.close();
+            System.out.println("Object deserialized: " + deserializedObject);
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
 }
